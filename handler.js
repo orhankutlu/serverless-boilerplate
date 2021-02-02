c31f99d7e0a8c493ec31b425b3ac936025f2b78f
Orhan Kutlu <orhankutlu@orhan-mac.local>
2021-02-02 17:41:23 +0300

    added validator

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 const responser = require('./responser');
 const config = require('./config');
 const eventParser = require('./utils/eventParser');
+const validator = require('./utils/validator');
 
 module.exports.action = async (event) => {
   const body = eventParser(event);
